8610a62bc3bac0d901228b1c6ddec453b1cbc2ad
Yasser A. Noori <prelistimothy@gmail.com>
2024-10-19 15:33:15 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Returns Fill in.
+     * @param name Fill in??
+     */
+    void setCurrentUser(String name);
 }
